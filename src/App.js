c2b68b70c68849662fa08e51c6dafb3a8d51df73
Zestymcspicy <halbeckerman@yahoo.com>
2019-10-14 00:22:46 -0500

    tasks are being added to list

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ date: "2019-10-13T04:02:09.438Z",
 email: "HopsTheDog@dogmail.com",
 isAdmin: false,
 name: "hop",
-password: "$2a$10$MODB9gRxtDtbScAefUyHpujcGZ9LDCNBXuR2cwcwCpUAy.cfOtxSC",
-tasks: [],
+tasks: [{name: "cat"}, {name: "face"}],
 __v: 0,
 _id: "5da2a1c12b83b52660df59c5"})
   const [location, setLocation] = useState('profile');
